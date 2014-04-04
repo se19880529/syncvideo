e019863e720e85931c863f2d5e80a317178919d6
supereagle <se19880529@gmail.com>
2014-04-04 18:57:58 +0800

    完善件下载

--- DownloaderTestProj/DownloaderTest/Server.cs
+++ DownloaderTestProj/DownloaderTest/Server.cs
@@ -7,6 +7,72 @@ using NET;
 
 namespace FileService
 {
+    
+    class ServerProtocol
+    {
+        public int type;
+        public ServerProtocol(int tp) { type = tp; }
+        
+
+        public virtual byte[] GetByte()
+        {
+            return BitConverter.GetBytes(type);
+        }
+
+        public virtual void FromByte(byte[] buffer)
+        {
+            type = BitConverter.ToInt32(buffer, 0);
+        }
+
+        public void Insert(ref int index, byte[] arr, byte[] dest)
+        {
+            Array.Copy(dest, 0, arr, index, dest.Length);
+            index += dest.Length;
+        }
+    }
+    class ShareInfoQuery : ServerProtocol
+    {
+        public const int TYPE = 1;
+        public ShareInfoQuery() : base(TYPE) { }
+    }
+    class ShareInfoRe : ServerProtocol
+    {
+        public const int TYPE = 2;
+        public int sectionid;
+        public int downloaded;
+        public long start;
+        public long end;
+        public byte[] md5;
+        public ShareInfoRe() : base(TYPE) { }
+        public override byte[] GetByte()
+        {
+            byte[] buffer = new byte[sizeof(int) * 3 + sizeof(long) * 2 + md5.Length];
+            int index = 0;
+            Insert(ref index, buffer, BitConverter.GetBytes(type));
+            Insert(ref index, buffer, BitConverter.GetBytes(sectionid));
+            Insert(ref index, buffer, BitConverter.GetBytes(downloaded));
+            Insert(ref index, buffer, BitConverter.GetBytes(start));
+            Insert(ref index, buffer, BitConverter.GetBytes(end));
+            Insert(ref index, buffer, md5);
+            return buffer;
+        }
+        public override void FromByte(byte[] buffer)
+        {
+            int index = 0;
+            type = BitConverter.ToInt32(buffer, index);
+            index += 4;
+            sectionid = BitConverter.ToInt32(buffer, index);
+            index += 4;
+            downloaded = BitConverter.ToInt32(buffer, index);
+            index += 4;
+            start = BitConverter.ToInt64(buffer, index);
+            index += 8;
+            end = BitConverter.ToInt32(buffer, index);
+            index += 8;
+            md5 = new byte[buffer.Length - index];
+            Array.Copy(buffer, index, md5, 0, buffer.Length - index);
+        }
+    }
     class Server
     {
         static ISocketManager _manager;
@@ -46,9 +112,11 @@ namespace FileService
             _manager.Recv(socket, buffer, buffer.Length);
             switch (buffer[0])
             {
-                case 0:         //ask for Share infomation
+             //   case 0:         //ask for Share infomation
                     
             }
         }
+
+
     }
 }
